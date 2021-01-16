ffbf88d031d352c5045e9dc455f3b900ac7ee8b5
C33J4Y <cherrera2018@fau.edu>
2021-01-16 21:42:36 +0000

    Reversal algorithm. Swap first and last digit. Move inwards until array is reversed.

--- main.cpp
+++ main.cpp
@@ -1,5 +1,38 @@
 #include <iostream>
 
+using namespace std;
+
+int* arrayReversal(int n, int array[]){
+
+ 
+
+   static int a[4];
+   int temp;
+
+  for (int k = 0; k < n; k++){
+    a[k] =  array[k];
+  } 
+
+  //Reversal Algorithm
+  for (int i = 0; i < n/2; i++){
+    temp = a[n-i-1];
+    a[n-i-1] = a[i];
+    a[i] = temp;
+
+  }
+
+  for (int j = 0; j < n; j++){
+    cout << a[j] << endl;
+  }
+
+  return a;
+
+}
+
 int main() {
-  std::cout << "Hello World!\n";
+
+  int n = 5;
+  int a[5] = {4,3,2,1,5};
+  
+  arrayReversal(n, a);
 }
